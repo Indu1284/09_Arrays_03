21e4927fa979301536660cb846daeb9ce587aa30
Indu <indurevathy@gmail.com>
2021-08-04 12:11:34 +0200

    updated funktion

--- challenge_arr_01.js
+++ challenge_arr_01.js
@@ -17,7 +17,11 @@ const tags = ["html","head","head","body","h1","h1","p","p","p","p","ul","li","l
 // Modul: HTML-String erzeugen | Test
 ausgabe(getHTML());
 function getHTML(){
-	
+	let htmlstr ="";
+    for(let i=0 ; i <tags.length ; i++){
+        htmlstr += tags[i];
+}       
+return htmlstr;
 }
 
 // Modul: Ausgabe | Test
